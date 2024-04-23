d98f20379282bc04e6a7cd0bea8d3ce0faf81a2d
CabreraGarciaEduardoEmmanuel <143464606+CabreraGarciaEduardoEmmanuel@users.noreply.github.com>
2024-04-23 01:47:37 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
-run : bin/tron
-	./bin/tron
-
 bin/tron : src/main.cpp
 	g++ -o bin/tron src/main.cpp -Iinclude -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
+	
+run : bin/tron
+	./bin/tron
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,28 @@
 #include <thread> //HILOSDE PROCESOS
 #include <fstream> // flujos de aechivo
 #include <string> //flujos entrada y salida
+#include <Dibujo.hpp>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    ifstream archivo;
-    archivo.open("./assets/images/moto.txt");
-    ostringstream buffer;
-    string moto;
-    moto << archivo.rdbuf();
-    moto = buffer;
-
-    archivo.close();
-
+   Dibujo moto("./assts/images/moto.txt");
+   Dibujo bloque("./assets/images/bloque.txt");
 
     int fotograma = 0;
     while (true)
     {
+
         this_thread::sleep_for(0.1s);
         fotograma++;
-        Element personaje = spinner(21, fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
-        Element eMoto = text(moto);
-        Element dibujo = hbox({personaje, eMoto});
-        Screen pantalla = Screen ::Create(Dimension::Full());
-        Render(pantalla, dibujo);
+        Element personaje = spinner (21,fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
+        Element eMoto = moto;
+        Element eBloque = bloque;
+        Element dibujo = hbox({personaje , moto.GetElement(), bloque,GetElement()});
+        Screen pantalla = Screen::Create(Dimension::Full());
+        Render(pantalla,dibujo);
         pantalla.Print();  
         cout<<pantalla.ResetPosition();
     }
